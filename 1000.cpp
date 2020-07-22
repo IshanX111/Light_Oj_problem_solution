ac3b76f554df46d5ef5a26379a429cd8a627d09b
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2020-07-23 04:58:24 +0600

    Add files via upload

---
+++ 1000.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int t,i,a,b;
+    cin>>t;
+    for(i=1;i<=t;i++){
+        cin>>a>>b;
+        printf("Case %d: %d\n",i,a+b);
+    }
+
+
+
+}
