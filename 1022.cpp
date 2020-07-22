2f835b238a0dc9cae713773f649502044541d128
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2020-07-23 04:58:38 +0600

    Add files via upload

---
+++ 1022.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define PI 2*acos(0.0)
+using namespace std;
+int main(){
+
+    int t,i;
+    double r;
+    cin>>t;
+    for(i=1;i<=t;i++){
+        cin>>r;
+        double res=((2*r)*(2*r))-PI*r*r;
+        printf("Case %d: %.2f\n",i,res);
+
+    }
+
+
+
+
+
+}
